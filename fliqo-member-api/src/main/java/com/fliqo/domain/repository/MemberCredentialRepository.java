f6ac8eb5f8cf1d7d92d2d19b0475f5921475a5c8
hyeminleeee <wds5329@naver.com>
2025-10-01 16:33:02 +0900

    ✨ feat: 이메일/자격증명 조회 메서드 보강

--- fliqo-member-api/src/main/java/com/fliqo/domain/repository/MemberCredentialRepository.java
+++ fliqo-member-api/src/main/java/com/fliqo/domain/repository/MemberCredentialRepository.java
@@ -1,7 +1,11 @@
 package com.fliqo.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fliqo.domain.entity.MemberCredential;
 
-public interface MemberCredentialRepository extends JpaRepository<MemberCredential, Long> {}
+public interface MemberCredentialRepository extends JpaRepository<MemberCredential, Long> {
+    Optional<MemberCredential> findByMemberId(Long memberId);
+}
--- fliqo-member-api/src/main/java/com/fliqo/domain/repository/MemberRepository.java
+++ fliqo-member-api/src/main/java/com/fliqo/domain/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package com.fliqo.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fliqo.domain.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
