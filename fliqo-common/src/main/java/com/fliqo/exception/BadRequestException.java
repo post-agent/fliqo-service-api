5f5661b44c29955b7f1a9cfd8d821c0f1906d240
ImJWoo <jeongu8807@daum.net>
2025-10-10 21:39:45 +0900

    ✨ feat(common): 잘못된 요청 예외 BadRequestException 추가

---
+++ fliqo-common/src/main/java/com/fliqo/exception/BadRequestException.java
@@ -0,0 +1,18 @@
+package com.fliqo.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * 잘못된 요청 예외 (400 Bad Request).
+ * 클라이언트의 요청이 잘못되었거나 유효하지 않을 때 발생.
+ */
+public class BadRequestException extends BaseException {
+
+    public BadRequestException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public BadRequestException(String message) {
+        super(HttpStatus.BAD_REQUEST, "BAD_REQUEST", message);
+    }
+}
