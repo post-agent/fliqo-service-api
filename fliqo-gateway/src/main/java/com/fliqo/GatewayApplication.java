669e75bec28f56f79cb280cff7d54f5e43bab487
ImJWoo <jeongu8807@daum.net>
2025-10-17 15:56:44 +0900

    🔧 chore(gateway/config): GatewayApplication에 공통 JwtProperties 스캔 설정

--- fliqo-gateway/src/main/java/com/fliqo/GatewayApplication.java
+++ fliqo-gateway/src/main/java/com/fliqo/GatewayApplication.java
@@ -1,8 +1,11 @@
 package com.fliqo;
 
+import com.fliqo.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+//@ConfigurationPropertiesScan(basePackageClasses = JwtProperties.class)
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
