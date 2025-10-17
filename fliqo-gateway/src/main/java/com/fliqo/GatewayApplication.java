407a2dc7d33fcf163647e9b17be10ff930a12bac
ImJWoo <jeongu8807@daum.net>
2025-10-17 21:37:17 +0900

    🔧 chore(gateway): @ConfigurationPropertiesScan으로 JwtProperties 바인딩 활성화

--- fliqo-gateway/src/main/java/com/fliqo/GatewayApplication.java
+++ fliqo-gateway/src/main/java/com/fliqo/GatewayApplication.java
@@ -4,8 +4,10 @@ import com.fliqo.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-//@ConfigurationPropertiesScan(basePackageClasses = JwtProperties.class)
+@ConfigurationPropertiesScan(basePackageClasses = JwtProperties.class)
+@EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
