81988ce985dbe8a0c26d5522183f718652bf386a
ImJWoo <jeongu8807@daum.net>
2025-10-17 15:54:59 +0900

    ✨ feat(member-api/config): AuthProperties(auth.*) 추가 및 ConfigurationProperties 스캔 설정

--- fliqo-member-api/src/main/java/com/fliqo/MemberApiApplication.java
+++ fliqo-member-api/src/main/java/com/fliqo/MemberApiApplication.java
@@ -1,8 +1,14 @@
 package com.fliqo;
 
+import com.fliqo.config.AuthProperties;
+import com.fliqo.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@ConfigurationPropertiesScan(basePackageClasses = JwtProperties.class)
+@EnableConfigurationProperties(AuthProperties.class)
 @SpringBootApplication
 public class MemberApiApplication {
     public static void main(String[] args) {
